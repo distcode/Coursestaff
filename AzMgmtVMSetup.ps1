77022a9eac5351b1ca0e619a99aefff60d35b5ef
distcode <office@dist.at>
2021-03-06 13:13:50 +0100

    Update AzMgmtVMSetup.ps1

--- AzMgmtVMSetup.ps1
+++ AzMgmtVMSetup.ps1
@@ -1,25 +1,29 @@
 #
 # Install PowerShell 7.0
 #
-Invoke-Expression "& { $(Invoke-RestMethod https://aka.ms/install-powershell.ps1) } -UseMSI -Quiet -AddExplorerContextMenu -EnablePSRemoting"
+# Invoke-Expression -Command "& { $(Invoke-RestMethod https://aka.ms/install-powershell.ps1) } -UseMSI -Quiet -AddExplorerContextMenu -EnablePSRemoting" 
+$cred = New-Object -TypeName pscredential -ArgumentList 'localadmin',(ConvertTo-SecureString -Force -AsPlainText -String 'securePa$$w0rd')
+Invoke-Command -Scriptblock { Invoke-Expression "& { $(Invoke-RestMethod https://aka.ms/install-powershell.ps1) } -UseMSI -Quiet -AddExplorerContextMenu -EnablePSRemoting" } -Credential $cred
 
 #
 # Install Azure CLI
 #
-Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile "$env:Temp\AzureCLI.msi"
-Start-Process msiexec.exe -Wait -ArgumentList "/I $env:Temp\AzureCLI.msi /quiet"
-Remove-Item "$env:Temp\AzureCLI.msi" -Force
+Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile "$env:Temp\AzureCLI.msi" -Credential $cred
+Start-Process msiexec.exe -Wait -ArgumentList "/I $env:Temp\AzureCLI.msi /quiet" -Credential $cred
+Remove-Item "$env:temp\AzureCLI.msi" -Force -Credential $cred
 
 #
 # Install Azure PowerShell Modules
 #
-& 'C:\Program Files\PowerShell\7\pwsh.exe' -command { Install-Module Az -Force } 
+& 'C:\Program Files\PowerShell\7\pwsh.exe' -command { Install-Module Az -Scope AllUsers -Force } 
 
 #
 # Install VSCode
 #
-& 'C:\Program Files\PowerShell\7\pwsh.exe' -command { Save-Script -Path c:\Temp -Name Install-VSCode -Force }
-& 'C:\Program Files\PowerShell\7\pwsh.exe' -command { C:\Temp\Install-VSCode.ps1 -EnableContextMenus -AdditionalExtensions @('ms-vscode.powershell','msazurermtools.azurerm-vscode-tools','ms-vscode.azurecli') > 2>&1 }
+# & 'C:\Program Files\PowerShell\7\pwsh.exe' -command { Save-Script -Path c:\Temp -Name Install-VSCode -Force }
+# & 'C:\Program Files\PowerShell\7\pwsh.exe' -command { C:\Temp\Install-VSCode.ps1 -buildEdition 'Stable-System' -EnableContextMenus -AdditionalExtensions @('ms-vscode.powershell','msazurermtools.azurerm-vscode-tools','ms-vscode.azurecli') *>&1 }
+Save-Script -Path $Env:Temp -Name Install-VSCode -Force 
+& "$env:temp\Install-VSCode.ps1" -buildEdition 'Stable-System' -EnableContextMenus
 
 #
 # Install Chrome
