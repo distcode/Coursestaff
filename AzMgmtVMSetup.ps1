7b98fe1e9b62138b93af441fb97725b540238164
distcode <office@dist.at>
2021-03-07 09:47:01 +0100

    Update AzMgmtVMSetup.ps1

--- AzMgmtVMSetup.ps1
+++ AzMgmtVMSetup.ps1
@@ -1,44 +1,77 @@
+
+$ErrorActionPreference = 'Stop'
+
 #
 # Install PowerShell 7.0
 #
-# Invoke-Expression -Command "& { $(Invoke-RestMethod https://aka.ms/install-powershell.ps1) } -UseMSI -Quiet -AddExplorerContextMenu -EnablePSRemoting" 
-$cred = New-Object -TypeName pscredential -ArgumentList 'localadmin', (ConvertTo-SecureString -Force -AsPlainText -String 'securePa$$w0rd')
-Invoke-Command -Scriptblock { Invoke-Expression "& { $(Invoke-RestMethod https://aka.ms/install-powershell.ps1) } -UseMSI -Quiet -AddExplorerContextMenu -EnablePSRemoting" }
+try {
+    # Invoke-Expression -Command "& { $(Invoke-RestMethod https://aka.ms/install-powershell.ps1) } -UseMSI -Quiet -AddExplorerContextMenu -EnablePSRemoting" 
+    $cred = New-Object -TypeName pscredential -ArgumentList 'localadmin', (ConvertTo-SecureString -Force -AsPlainText -String 'securePa$$w0rd')
+    Invoke-Command -Scriptblock { Invoke-Expression "& { $(Invoke-RestMethod https://aka.ms/install-powershell.ps1) } -UseMSI -Quiet -AddExplorerContextMenu -EnablePSRemoting" }
+}
+catch {
+    Out-Host 'Error PS7 installation'
+}
 
 #
 # Install Azure CLI
 #
-
+try {
     Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile "$env:Temp\AzureCLI.msi" 
     Start-Process msiexec.exe -Wait -ArgumentList "/I $env:Temp\AzureCLI.msi /quiet"
     Remove-Item "$env:temp\AzureCLI.msi" -Force -Credential $cred
-
+}
+catch {
+    Out-Host 'Error Az CLI Installation'
+}
 
 #
 # Install Azure PowerShell Modules
 #
-& 'C:\Program Files\PowerShell\7\pwsh.exe' -command { Install-Module Az -Scope AllUsers -Force } 
+try {
+    & 'C:\Program Files\PowerShell\7\pwsh.exe' -command { Install-Module Az -Scope AllUsers -Force } 
+}
+catch {
+    Out-Host 'Error PS Module Installation'
+}
 
 #
 # Install VSCode
 #
-# & 'C:\Program Files\PowerShell\7\pwsh.exe' -command { Save-Script -Path c:\Temp -Name Install-VSCode -Force }
-# & 'C:\Program Files\PowerShell\7\pwsh.exe' -command { C:\Temp\Install-VSCode.ps1 -buildEdition 'Stable-System' -EnableContextMenus -AdditionalExtensions @('ms-vscode.powershell','msazurermtools.azurerm-vscode-tools','ms-vscode.azurecli') *>&1 }
-Save-Script -Path $Env:Temp -Name Install-VSCode -Force 
-& "$env:temp\Install-VSCode.ps1" -buildEdition 'Stable-System' -EnableContextMenus
+
+try { 
+    # & 'C:\Program Files\PowerShell\7\pwsh.exe' -command { Save-Script -Path c:\Temp -Name Install-VSCode -Force }
+    # & 'C:\Program Files\PowerShell\7\pwsh.exe' -command { C:\Temp\Install-VSCode.ps1 -buildEdition 'Stable-System' -EnableContextMenus -AdditionalExtensions @('ms-vscode.powershell','msazurermtools.azurerm-vscode-tools','ms-vscode.azurecli') *>&1 }
+    Save-Script -Path $Env:Temp -Name Install-VSCode -Force 
+    & "$env:temp\Install-VSCode.ps1" -buildEdition 'Stable-System' -EnableContextMenus
+}
+catch {
+    Out-Host 'Error VSCode installation.'
+}
 
 #
 # Install Chrome
 #
-$Installer = "$env:temp\chrome_installer.exe"
-$url = 'http://dl.google.com/chrome/install/375.126/chrome_installer.exe'
-Invoke-WebRequest -Uri $url -OutFile $Installer -UseBasicParsing
-Start-Process -FilePath $Installer -Args '/silent /install' -Wait
-Remove-Item -Path $Installer  
+
+try {
+    $Installer = "$env:temp\chrome_installer.exe"
+    $url = 'http://dl.google.com/chrome/install/375.126/chrome_installer.exe'
+    Invoke-WebRequest -Uri $url -OutFile $Installer -UseBasicParsing
+    Start-Process -FilePath $Installer -Args '/silent /install' -Wait
+    Remove-Item -Path $Installer  
+}
+catch {
+    Out-Host 'Error Crome installation'
+}
 
 #
 # Install Edge
 #
-Save-Script -Name Download-MicrosoftEdge -Path $env:Temp
-& "$env:Temp\Download-MicrosoftEdge.ps1" -Folder $env:Temp -Channel Stable
-Start-Process msiexec.exe -Wait -ArgumentList "/I $env:Temp\MicrosoftEdgeEnterpriseX64 /quiet /noRestart"
+try {
+    Save-Script -Name Download-MicrosoftEdge -Path $env:Temp
+    & "$env:Temp\Download-MicrosoftEdge.ps1" -Folder $env:Temp -Channel Stable
+    Start-Process msiexec.exe -Wait -ArgumentList "/I $env:Temp\MicrosoftEdgeEnterpriseX64 /quiet /noRestart"
+}
+catch {
+    Out-Host 'Error Edge installation'
+}
