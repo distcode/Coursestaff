d1d712d7d362ea8b6f4f40ce5c4217be2d02dd40
DI Thomas Schleich <office@dist.at>
2024-12-18 20:16:48 +0100

    new version uploaded

---
+++ Configure-VMSS-Shop.ps1
@@ -0,0 +1,19 @@
+
+$htmlDoc = @"
+<!DOCTYPE html>
+<title>Demo Traffic Managment</title>
+<body>
+<br />
+<h1 style="color:Red;text-align:center;">VMSS - HOME</h1>
+<p style="text-align:center";>This is instance $($env:computername).</p>
+</body>
+"@
+
+if ( (Get-Windowsfeature -Name 'Web-Server' ).InstallState -ne 'Installed' ) 
+{
+    Install-Windowsfeature -Name 'Web-Server'
+}
+
+Set-Location c:\inetpub\wwwroot
+Remove-Item *.* -Force
+Set-Content -Value $htmlDoc -Path .\index.html
